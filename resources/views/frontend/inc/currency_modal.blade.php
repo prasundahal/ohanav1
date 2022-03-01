274439249b1b14e037076eb00f8c0fb508b62af7
Muniraj Rajbanshi <munirajrajbanshi@gmail.com>
2022-03-01 10:22:42 +0545

    testimonial done

--- resources/views/frontend/inc/currency_modal.blade.php
+++ resources/views/frontend/inc/currency_modal.blade.php
@@ -7,7 +7,7 @@ $npr_curency = App\Models\Currency::where('currency_code', 'NPR')->first()->rate
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="currencyModalLabel">Modal title</h5>
+                <h5 class="modal-title" id="currencyModalLabel">Convert Currency Here</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
